661e54ab971104249ddda07d7cbc41d713a14b20
Onkilo <onkilo12@gmail.com>
2020-10-09 18:10:29 -0500

    Se agregó babel y npm watch

---
+++ Athenas.MVCUI/Scripts/es5/athenasNet.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var suma = function suma(a, b) {
+  return a + b;
+};
+
+var a = 5;
+var b = 7;
+console.log(suma(a, b));
+//# sourceMappingURL=athenasNet.js.map
---
+++ Athenas.MVCUI/Scripts/es6/athenasNet.js
@@ -0,0 +1,9 @@
+﻿
+const suma = (a, b) => {
+    return a + b;
+}
+
+const a = 5;
+const b = 7;
+
+console.log(suma(a, b));
