f2b5f485a9b99b729a7693a8ac52aab2bed4e4cd
Onkilo <onkilo12@gmail.com>
2020-10-09 20:45:29 -0500

    Se inicio crud de categoria

--- Athenas.MVCUI/Scripts/es5/athenasNet.js
+++ Athenas.MVCUI/Scripts/es5/athenasNet.js
@@ -1,10 +1,87 @@
 "use strict";
 
-var suma = function suma(a, b) {
-  return a + b;
-};
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-var a = 5;
-var b = 7;
-console.log(suma(a, b));
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+(function () {
+  var MVC_URL_BASE = 'http://localhost:62622/';
+  var ACCION_MOSTRAR_SPINNER = 'mostrar';
+  var ACCION_OCULTAR_SPINNER = 'mostrar';
+
+  var llamadaApi = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(opciones) {
+      var opcionsDefecto, respuesta;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              opcionsDefecto = {
+                beforeSend: function beforeSend() {//manejaSpinner(ACCION_MOSTRAR_SPINNER);
+                },
+                contentType: 'application/json',
+                data: {},
+                type: 'GET'
+              };
+              _context.prev = 1;
+              _context.next = 4;
+              return $.ajax(_objectSpread(_objectSpread(_objectSpread({}, opcionsDefecto), opciones), {}, {
+                url: MVC_URL_BASE + opciones.url
+              }));
+
+            case 4:
+              respuesta = _context.sent;
+
+              if (!(respuesta.Codigo !== 200)) {
+                _context.next = 7;
+                break;
+              }
+
+              throw respuesta;
+
+            case 7:
+              return _context.abrupt("return", respuesta);
+
+            case 10:
+              _context.prev = 10;
+              _context.t0 = _context["catch"](1);
+              throw _context.t0;
+
+            case 13:
+              _context.prev = 13;
+              return _context.finish(13);
+
+            case 15:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee, null, [[1, 10, 13, 15]]);
+    }));
+
+    return function llamadaApi(_x) {
+      return _ref.apply(this, arguments);
+    };
+  }();
+
+  var manejaSpinner = function manejaSpinner(estado) {
+    var spinner = document.querySelector('.spinner-container');
+    if (estado === 'mostrar') spinner.style.display = 'flex';else spinner.style.display = 'none';
+  };
+
+  window.AthenasNet = {
+    llamadaApi: llamadaApi,
+    manejaSpinner: manejaSpinner
+  };
+})();
+
+window.addEventListener('load', function () {
+  $('#general-toast').toast();
+});
 //# sourceMappingURL=athenasNet.js.map
---
+++ Athenas.MVCUI/Scripts/es6/categoria.js
@@ -0,0 +1,101 @@
+﻿
+
+const crearCategoria = async (categoria) => {
+    try {
+
+        const respuesta = await AthenasNet.llamadaApi({
+            type: 'POST',
+            data: JSON.stringify(categoria),
+            url: 'Categoria/Crear'
+        })
+        //activar el toast
+        console.log(respuesta);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+const actualizarCategoria = async (categoria) => {
+    try {
+
+        const respuesta = await AthenasNet.llamadaApi({
+            type: 'POST',
+            data: JSON.stringify(categoria),
+            url: 'Categoria/Actualizar'
+        })
+        //activar el toast
+        console.log(respuesta);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+const listarCategoria = async (filtros) => {
+    try {
+
+        const filtrosDefecto = {
+            Descripcion: '',
+            ...filtros
+        }
+
+        const respuesta = await AthenasNet.llamadaApi({
+            data: filtrosDefecto,
+            url: 'Categoria/Listar'
+        })
+        //activar el toast
+        console.log(respuesta);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+const buscarCategoria = async (id) => {
+    try {
+        const respuesta = await AthenasNet.llamadaApi({
+            data: { Id: id },
+            url: 'Categoria/Obtener'
+        })
+        //activar el toast
+        console.log(respuesta);
+        $('#modal-categoria').modal('hide');
+        //Limpiar campos
+    }
+    catch (err) {
+        //mensaje de error en el toast
+        console.error(err);
+    }
+}
+
+const eliminarCategoria = async (id) => {
+    try {
+        const respuesta = await AthenasNet.llamadaApi({
+            data: { Id: id },
+            url: 'Categoria/Eliminar'
+        })
+        //activar el toast
+        console.log(respuesta);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+
+
+window.addEventListener('load', () => {
+
+    document.querySelector('#form-categoria').addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        const categoria = {
+            Descripcion: txtDescripcion.value,
+            Id: parseInt(document.querySelector('#hdn-id').value),
+        };
+
+        buscarCategoria(3005)
+    })
+
+
+})
